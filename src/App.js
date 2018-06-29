e2723818b3001057f60c1e736ea033ae7c7bf537
Non <non.devtech@gmail.com>
2018-06-30 02:09:07 +0700

    Tickers Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Tickers from './components/Tickers.js';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h2>Cryptocurrency Ticker</h2>
         </header>
+        <Tickers />
       </div>
     );
   }
